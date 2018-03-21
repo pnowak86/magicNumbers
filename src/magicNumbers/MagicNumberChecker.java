f303531a894be2a64fca36f3e6d642bc7421909c
pinq <p_nowak86@poczta.fm>
2018-03-21 19:15:49 +0100

    added check for GIF Magic Number, working

--- src/magicNumbers/MagicNumberChecker.java
+++ src/magicNumbers/MagicNumberChecker.java
@@ -12,15 +12,15 @@ public class MagicNumberChecker {
 
             int fileSignature = input.readInt();
             input.reset();
-            byte[] b = new byte[8];
-            input.read(b, 0, 8);
+            boolean isGif = checkIfFileIsGif(file);
+            
             if (fileSignature == 0xffd8ffe0) {
                 System.out.println("File is a JPG");
                 return true;
             } else if (fileSignature == 0x89504E47) {
                 System.out.println("File is a PNG");
                 return true;
-            } else if (Arrays.equals(b, new BigInteger("474946383761", 16).toByteArray())) {
+            } else if (isGif) {
                 System.out.println("File is a GIF");
                 return true;
             } else if (fileSignature == 0x464F524D) {
@@ -37,5 +37,19 @@ public class MagicNumberChecker {
         return false;
     }
 
+
+    public boolean checkIfFileIsGif(File file) {
+        byte[] b = new byte[6];
+
+        try (DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) {
+            input.read(b, 0, 6);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (b[0] == 'G' && b[1] == 'I' && b[2] == 'F' && b[3] == '8' && (b[4] == '7' || b[4] == '9') && b[5] != 'a') {
+            return true;
+        }
+        return false;
+    }
 
 }
