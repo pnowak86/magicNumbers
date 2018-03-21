1967c0d6bae59d2097ca388ea5045407e87249c4
pinq <p_nowak86@poczta.fm>
2018-03-21 18:37:09 +0100

    trying to add check for gif

--- src/magicNumbers/MagicNumberChecker.java
+++ src/magicNumbers/MagicNumberChecker.java
@@ -1,6 +1,8 @@
 package magicNumbers;
 
 import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
 
 public class MagicNumberChecker {
 
@@ -9,12 +11,17 @@ public class MagicNumberChecker {
         try (DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) {
 
             int fileSignature = input.readInt();
-
+            input.reset();
+            byte[] b = new byte[8];
+            input.read(b, 0, 8);
             if (fileSignature == 0xffd8ffe0) {
                 System.out.println("File is a JPG");
                 return true;
             } else if (fileSignature == 0x89504E47) {
                 System.out.println("File is a PNG");
+                return true;
+            } else if (Arrays.equals(b, new BigInteger("474946383761", 16).toByteArray())) {
+                System.out.println("File is a GIF");
                 return true;
             } else if (fileSignature == 0x464F524D) {
                 System.out.println("File is a PNG");
@@ -30,5 +37,5 @@ public class MagicNumberChecker {
         return false;
     }
 
-    
+
 }
