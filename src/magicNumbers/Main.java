6897c242c4ccc25bb064ae8f2848ffe48cbfcd09
pinq <p_nowak86@poczta.fm>
2018-03-21 19:35:18 +0100

    little cleanup in main

--- src/magicNumbers/Main.java
+++ src/magicNumbers/Main.java
@@ -10,13 +10,17 @@ public class Main {
         MagicNumberChecker magicNumberChecker = new MagicNumberChecker();
         ExtensionChecker extensionChecker = new ExtensionChecker();
         Scanner scanner = new Scanner(System.in);
-        
+
         boolean running = true;
 
         System.out.println("Welcome to file validator, please enter a path to file");
         String filePath = scanner.nextLine();
         File file = new File(filePath);
-        while (running) {
+        boolean isFileExist = file.isFile();
+        if(isFileExist==false){
+            System.out.println("File do not exist!");
+        }
+        while (running && isFileExist) {
             System.out.println("Please check how you want to check your file:");
             System.out.println("1. Type 'mg' to check by magic number \n" +
                     "2. Type 'ext' to check extension\n" +
